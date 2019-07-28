de617bdd3f64475a7780549e0ed47f503f7721f5
cmseeling <cmseeling@gmail.com>
2019-07-28 17:00:02 -0500

    added typescript and linting

---
+++ src/components/HelloComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <span>{{count}}</span>
+    <button @click="onClick">Click me</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'HelloComponent',
+  data: () => {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    onClick() {
+      this.count++;
+    }
+  }
+});
+</script>
