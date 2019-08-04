8fcf890fe8aadda0ef6f8a978dc952f8cefd2f67
cmseeling <cmseeling@gmail.com>
2019-08-03 23:51:23 -0500

    added fonts and first colors

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-content :class="`${$route.params.guestType} main-content`">
+  <v-content :class="`${$route.params.guestType}-main-content`">
     <v-container fill-height>
       <v-layout column>
         <v-flex shrink>
           <v-container text-center>
             <v-layout row align-center>
               <v-flex v-for="link in navLinks" :key="link.id">
-                <router-link :to="link.path">{{link.name}}</router-link>
+                <router-link :to="link.path" :class="`header-link ${textColor}`">{{link.name}}</router-link>
               </v-flex>
             </v-layout>
           </v-container>
@@ -30,16 +30,35 @@ export default Vue.extend({
         { id: 1, name: 'Home', path: `/${this.$route.params.guestType}` },
         { id: 2, name: 'Schedule', path: `/${this.$route.params.guestType}/schedule` },
         { id: 3, name: 'Travel & Accommodation', path: `/${this.$route.params.guestType}/accommodations` },
-        { id: 4, name: 'RSVP', path: `/${this.$route.params.guestType}/rsvp` },
+        { id: 4, name: 'R.S.V.P.', path: `/${this.$route.params.guestType}/rsvp` },
         { id: 5, name: 'FAQs', path: `/${this.$route.params.guestType}/faqs` },
       ]
     };
+  },
+  computed: {
+    textColor() {
+      if (this.$route.params.guestType.toLowerCase() === 'family') {
+        return 'indigo--text darken-4';
+      } else {
+        // return 'red--text accent-4';
+        return 'blue-grey--text darken-4';
+      }
+    }
   }
 });
 </script>
 
 <style>
-  .main-content {
+  .friends-main-content {
     background-color: lightblue;
+  }
+
+  .family-main-content {
+    background-color: rgba(128,0,0,0.45);
+  }
+
+  .header-link {
+    font-family: 'Courgette', cursive;
+    font-size: 1.5rem;
   }
 </style>
--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,6 +1,18 @@
 <template>
   <Layout>
-    <div>Schedule</div>
+    <v-container text-center>
+      <v-layout row>
+        <v-flex/>
+        <v-flex shrink>
+          <v-layout column class="info-text-container">
+            <v-flex ma-5 text-left>
+              <p class="info-text">We will be at Bryant Lake Regional Park pavilion, which is located in Eden Prairie, a suburb of Minneapolis.</p>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+        <v-flex/>
+      </v-layout>
+    </v-container>
   </Layout>
 </template>
 
@@ -14,4 +26,17 @@ export default Vue.extend({
   }
 });
 </script>
+
+<style scoped>
+  .info-text-container {
+    border: 1px black solid;
+    background-color: rgba(255,255,255,0.6);
+  }
+
+  .info-text {
+    /* font-family: serif;
+    font-size: 1.25rem; */
+    font-family: 'Montserrat', sans-serif;
+  }
+</style>
 
