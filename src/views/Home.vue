55396bfdf7cbb77e2b772fb7b57dbc7c16a75a30
cmseeling <30421079+cmseeling@users.noreply.github.com>
2019-08-16 13:25:34 -0500

    removed staging banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,6 @@
       <v-layout column>
         <v-flex>
           <h1 :class="headerClass">Welcome! Our wedding reception with our {{$route.params.guestType}} will be {{date}}.</h1>
-          <h1 style="background-color: orange">Staging Environment - this banner will not appear on live site</h1>
         </v-flex>
         <v-flex v-if="$vuetify.breakpoint.mdAndUp" mt-3>
           <v-img src="/images/city_museum_cropped.jpg" aspect-ratio="3" contain/>
