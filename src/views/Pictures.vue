bc25ef07421dd2940afc4be12cc4341b4e216159
chris <cmseeling@gmail.com>
2020-11-27 13:07:40 -0600

    included all pictures

--- src/views/Pictures.vue
+++ src/views/Pictures.vue
@@ -11,17 +11,77 @@
             <v-flex ma-5>
               <v-row>
                 <v-col class="d-flex child-flex">
-                  <v-img src="/images/ceremonypics/ChrisandMalka-229.jpg"/>
-                  <v-img src="/images/ceremonypics/ChrisandMalka-147.jpg"/>
-                  <v-img src="/images/ceremonypics/ChrisandMalka-235.jpg"/>
-                  <v-img src="/images/ceremonypics/ChrisandMalka-210.jpg"/>
+                  <v-img src="/images/ceremonypics/ChrisandMalka-229.jpg" class="row-img"/>
+                  <v-img src="/images/ceremonypics/ChrisandMalka-147.jpg" class="row-img"/>
+                  <v-img src="/images/ceremonypics/ChrisandMalka-235.jpg" class="row-img"/>
+                  <v-img src="/images/ceremonypics/ChrisandMalka-210.jpg" class="row-img"/>
                 </v-col>
               </v-row>
             </v-flex>
             <v-flex ma-5>
               <v-row>
                 <v-col class="d-flex child-flex">
-                  <v-img src="/images/ceremonypics/ChrisandMalka3.jpg"/>
+                  <v-img src="/images/ceremonypics/ChrisandMalka3.jpg" max-height="620" contain/>
+                </v-col>
+              </v-row>
+            </v-flex>
+            <v-flex ma-5>
+              <v-row>
+                <v-col class="d-flex child-flex">
+                  <v-img src="/images/ceremonypics/balloons.jpg" class="row-img"/>
+                  <v-img src="/images/ceremonypics/cake.jpg" class="row-img"/>
+                  <v-img src="/images/ceremonypics/malka-computer.jpg" class="row-img"/>
+                  <v-img src="/images/ceremonypics/tshirts.jpg" class="row-img"/>
+                </v-col>
+              </v-row>
+            </v-flex>
+            <v-flex ma-5>
+              <v-row>
+                <v-col class="d-flex child-flex">
+                  <v-img src="/images/ceremonypics/camera1.jpg" max-height="620" contain/>
+                </v-col>
+              </v-row>
+            </v-flex>
+            <v-flex ma-5>
+              <v-row>
+                <v-col class="d-flex child-flex">
+                  <v-img src="/images/ceremonypics/signing.jpg" class="row-img"/>
+                  <v-img src="/images/ceremonypics/camera2.jpg" class="row-img"/>
+                  <v-img src="/images/ceremonypics/ceremony_bw.jpg" class="row-img"/>
+                  <v-img src="/images/ceremonypics/ceremony1.jpg" class="row-img"/>
+                </v-col>
+              </v-row>
+            </v-flex>
+            <v-flex ma-5>
+              <v-row>
+                <v-col class="d-flex child-flex">
+                  <v-img src="/images/ceremonypics/chris_malka_judy.jpg" class="row-img"/>
+                  <v-img src="/images/ceremonypics/ChrisandMalka-216.jpg" class="row-img"/>
+                  <v-img src="/images/ceremonypics/ChrisandMalka-222.jpg" class="row-img"/>
+                  <v-img src="/images/ceremonypics/ChrisandMalka-226.jpg" class="row-img"/>
+                </v-col>
+              </v-row>
+            </v-flex>
+            <v-flex ma-5>
+              <v-row>
+                <v-col class="d-flex child-flex">
+                  <v-img src="/images/ceremonypics/cutting-cake.jpg" max-height="620" contain/>
+                </v-col>
+              </v-row>
+            </v-flex>
+            <v-flex ma-5>
+              <v-row>
+                <v-col class="d-flex child-flex">
+                  <v-img src="/images/ceremonypics/ChrisandMalka-227.jpg" class="row-img"/>
+                  <v-img src="/images/ceremonypics/malkaandmom.jpg" class="row-img"/>
+                  <v-img src="/images/ceremonypics/seelingfamily.jpg" class="row-img"/>
+                </v-col>
+              </v-row>
+            </v-flex>
+            <v-flex ma-5>
+              <v-row>
+                <v-col class="d-flex child-flex">
+                  <v-img src="/images/ceremonypics/zoomcall.jpg" max-height="620" contain/>
                 </v-col>
               </v-row>
             </v-flex>
@@ -67,6 +127,10 @@ export default Vue.extend({
 
   .large-image {
     margin: 20px;
+  }
+
+  .row-img {
+    margin: 4px;
   }
 </style>
 
